885b10161bc7385d25db530fcccc094ce5d707aa
Richard Diamond <wichard@vitalitystudios.com>
2015-01-02 07:56:39 -0600

    Add an install script.

---
+++ install.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+cargo build --release && sudo cp target/pexe-runner /usr/local/bin/ && sudo sh -c 'echo ":pnacl:E::pexe::/usr/local/bin/pexe-runner:" > register' && echo "Good to go! You can now use pexes wherever you use regular binaries."
