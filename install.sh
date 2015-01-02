9edbf465f26efd729661752ae64b1ebaa9e07570
Richard Diamond <wichard@vitalitystudios.com>
2015-01-02 09:39:22 -0600

    >.< Fix more paths in install.sh.

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-cargo build --release && sudo cp target/release/pexe-runner /usr/local/bin/ && sudo sh -c 'echo ":pnacl:E::pexe::/usr/local/bin/pexe-runner:" > register' && echo "Good to go! You can now use pexes wherever you use regular binaries."
+cargo build --release && sudo cp target/release/pexe-runner /usr/local/bin/ && sudo sh -c 'echo ":pnacl:E::pexe::/usr/local/bin/pexe-runner:" > /proc/sys/fs/binfmt_misc/register' && echo "Good to go! You can now use pexes wherever you use regular binaries."
