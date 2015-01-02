97ad7d4eb899d15fd45722856ee8870c4ea2c16b
Richard Diamond <wichard@vitalitystudios.com>
2015-01-02 09:48:51 -0600

    Renovate ./install.sh.

--- install.sh
+++ install.sh
@@ -1,3 +1,12 @@
 #!/bin/sh
 
-cargo build --release && sudo cp target/release/pexe-runner /usr/local/bin/ && sudo sh -c 'echo ":pnacl:E::pexe::/usr/local/bin/pexe-runner:" > /proc/sys/fs/binfmt_misc/register' && echo "Good to go! You can now use pexes wherever you use regular binaries."
+(cargo build --release &&
+    echo "Copying bin to /usr/local/bin (prepare for sudo):" &&
+    sudo cp target/release/pexe-runner /usr/local/bin/ &&
+    echo "Done!") || exit 1;
+
+if [ ! -f /proc/sys/fs/binfmt_misc/pnacl ]; then
+    (sudo sh -c 'echo ":pnacl:E::pexe::/usr/local/bin/pexe-runner:" > /proc/sys/fs/binfmt_misc/register') || exit 1;
+fi
+
+echo "Good to go! You can now use pexes wherever you use regular binaries."
