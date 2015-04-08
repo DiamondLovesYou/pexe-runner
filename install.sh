4d959a214ef45105df4a57b401911821c14c7f65
Richard Diamond <wichard@vitalitystudios.com>
2015-04-08 17:52:52 -0500

    Fix install.sh so it will correctly re-register pexe_runner with binfmt_misc.

--- install.sh
+++ install.sh
@@ -2,11 +2,13 @@
 
 (cargo build --release &&
     echo "Copying bin to /usr/local/bin (prepare for sudo):" &&
-    sudo cp target/release/pexe-runner /usr/local/bin/ &&
+    sudo rm -f /usr/local/bin/pexe-runner &&
+    sudo cp target/release/pexe_runner /usr/local/bin/ &&
     echo "Done!") || exit 1;
 
-if [ ! -f /proc/sys/fs/binfmt_misc/pnacl ]; then
-    (sudo sh -c 'echo ":pnacl:E::pexe::/usr/local/bin/pexe_runner:" > /proc/sys/fs/binfmt_misc/register') || exit 1;
+if [ -f /proc/sys/fs/binfmt_misc/pnacl ]; then
+    (sudo sh -c 'echo "-1" > /proc/sys/fs/binfmt_misc/pnacl') || exit 1;
 fi
+(sudo sh -c 'echo ":pnacl:E::pexe::/usr/local/bin/pexe_runner:" > /proc/sys/fs/binfmt_misc/register') || exit 1;
 
 echo "Good to go! You can now use pexes wherever you use regular binaries."
